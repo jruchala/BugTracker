c552973ffaed9d4f1aa83662e78db9a07ab7fdb7
jruchala <jruchala@gmail.com>
2016-10-27 10:51:49 -0400

    add AdminUserViewModel.cs

---
+++ BugTracker/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using BugTracker.Models;
+using BugTracker.Helpers;
+
+namespace BugTracker.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        ApplicationDbContext db = new ApplicationDbContext();
+        UserRolesHelper helper = new UserRolesHelper();
+
+        // GET: Admin
+        [Authorize(Roles="Admin")]
+        public ActionResult Index()
+        {
+            List < AdminUserViewModel > = new List<AdminUserViewModel>();
+            return View();
+        }
+    }
+}
