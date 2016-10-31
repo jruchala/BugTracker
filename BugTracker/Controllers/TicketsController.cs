128586b9740ce302452392654fca659374db81bc
jruchala <jruchala@gmail.com>
2016-10-31 14:49:10 -0400

    delete Tickets/Delete methods from controller

--- BugTracker/Controllers/TicketsController.cs
+++ BugTracker/Controllers/TicketsController.cs
@@ -118,39 +118,9 @@ namespace BugTracker.Controllers
             return View(ticket);
         }
 
+        // NO Delete Ticket Method, per SRS
         // GET: Tickets/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Ticket ticket = db.Tickets.Find(id);
-            if (ticket == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ticket);
-        }
-
         // POST: Tickets/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Ticket ticket = db.Tickets.Find(id);
-            db.Tickets.Remove(ticket);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
