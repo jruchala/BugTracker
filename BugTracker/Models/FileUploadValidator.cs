84f8b035fcdd8daece0e9886abcf376c0abc11a1
jruchala <jruchala@gmail.com>
2016-11-17 12:00:53 -0500

    removed onload js from TicketAttachment Create and Edit views

--- BugTracker/Models/FileUploadValidator.cs
+++ BugTracker/Models/FileUploadValidator.cs
@@ -16,7 +16,7 @@ namespace BugTracker.Models
             if (file == null)
                 return false;
             // check size - file must be less than 2 MB and greater than 1 KB
-            if (file.ContentLength > 2 * 1024 * 1024 || file.ContentLength < 1024)
+            if (file.ContentLength > 2 * 1024 * 1024)
                 return false;
             // check for extensions of the file
             string fileExt = Path.GetExtension(file.FileName).ToLower();
